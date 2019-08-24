53ffc6447ce2d093b2f6709310894b6abb5c558d
Keighley McFarland <mcfarland.ke@gmail.com>
2019-08-24 16:20:26 +0200

    complete first draft styling of landing page, click animation not yet fixed on start button

---
+++ constants.js
@@ -0,0 +1,11 @@
+/*
+Colors
+dark eggplant: rgb(58,53,73)
+dark purple: rgb(47,49,92)
+white-smoke: rgb(239,242,245)
+pale purple: rgb(102,103,134)
+blue-black: rgb(6,9,19)
+medium purple: rgb(54,62,101)
+dark teal: rgb(33,51,77)
+dark dark purple: rgb(33,34,68)
+*/
